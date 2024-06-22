3e6a27f27a3f073c015d192df70bcff82e0146a9
Mahedi715 <mahedi615@gmail.com>
2024-06-22 18:02:51 +0600

    assignment Q2

---
+++ c/Loop/pattern_02.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int N, k = 0, s;
+    scanf("%d", &N);
+    s = N - 1;
+    for (int i = 1; i <= N; i++)
+    {
+        for (int j = 0; j < s; j++)
+        {
+            printf(" ");
+        }
+        for (int j = 0; j <= i; j++)
+        {
+            printf("%d", i);
+        }
+
+        k++;
+        s--;
+        printf("\n");
+    }
+}
